f3997c3a1a689bb39797cd9dff087f8bbc1cd2aa
QuertierDorian <54894000+QuertierDorian@users.noreply.github.com>
2019-09-05 17:03:54 +0200

    Implantation CSS
    Implantation de css avec du Media Queries

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/*var ny = [40.741895, -73.989300];
+
+var map = L.map('map').setView(ny, 6);
+L.tileLayer('https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png', {
+	maxZoom: 19
+}).addTo(map);
+
+var marker =L.marker(ny).addTo(map);
+
+marker.bindPopup('<h3> New York, USA. </h3>');*/
